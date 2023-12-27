b4c4472bbf9422fabbd99e9023a5829b69cc2b0f
JAPPLET <trianajuan970@gmail.com>
2023-12-26 21:23:01 -0500

    :tada: Eliminamos en el footer un link mal redireccionado

--- src/components/footer/FooterComponent.jsx
+++ src/components/footer/FooterComponent.jsx
@@ -5,11 +5,7 @@ const FooterComponent = () => {
     <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2023{" "}
-          <a href="https://flowbite.com/" className="hover:underline">
-            Juan David Triana Gómez
-          </a>
-          . Todos los derechos reservados.
+          © 2023 Juan David Triana Gómez. Todos los derechos reservados.
         </span>
       </div>
     </footer>
